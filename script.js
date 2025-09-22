b3b23c571d9887c2d822f523aec7a51ad866c367
ssamek <123518145+ssamek@users.noreply.github.com>
2025-09-22 15:13:54 +0000

    Base codes

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+}
